41532b387c957aab83156a7cdca24d6bbaba88e5
Aloui Mohamed Habib <mohamed-habib.aloui@proxym-it.com>
2019-04-27 00:45:05 +0100

    Chart without directives

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,50 +1,95 @@
-import { Component, OnInit, ViewChild } from "@angular/core";
-import { ChartDataSets, ChartOptions } from "chart.js";
-import { Color, BaseChartDirective, Label } from "ng2-charts";
+import {
+  Component,
+  OnInit,
+  ViewChild,
+  ElementRef,
+  AfterViewInit
+} from "@angular/core";
 import { HttpClient } from "@angular/common/http";
+import { ChartType, Chart } from "chart.js";
+import { SingleDataSet, Label, Color } from "ng2-charts";
+
 @Component({
   selector: "app-root",
   templateUrl: "./app.component.html",
   styleUrls: ["./app.component.scss"]
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit, AfterViewInit {
   title = "ChartJs";
   url = "http://www.somaku.com/todos";
-  public lineChartData: ChartDataSets[] = [];
-  public lineChartLabels: Label[] = [
-    "January",
-    "February",
-    "March",
-    "April",
-    "May",
-    "June",
-    "July"
+
+  public polarAreaLegend = true;
+  public polarAreaChartType: ChartType = "doughnut";
+  public bgColor: Color[] = [
+    {
+      // grey
+      backgroundColor:
+        "linear-gradient(to right, rgba(255,0,0,0), rgba(255,0,0,1))",
+      borderColor: "purple",
+      pointBackgroundColor: "rgba(148,159,177,1)",
+      pointBorderColor: "#fff",
+      pointHoverBackgroundColor: "#fff",
+      pointHoverBorderColor: "rgba(148,159,177,0.8)"
+    }
   ];
-
-  public lineChartType = "line";
-  public lineChartPlugins = [];
-
-  @ViewChild(BaseChartDirective) chart: BaseChartDirective;
+  @ViewChild("myChart") myChart: ElementRef;
 
   constructor(private http: HttpClient) {}
 
-  ngOnInit() { 
-      this.http.get(this.url).subscribe((todos: any) => {
-        let dataFiltred: any = todos.filter((item, key) => {
-          // console.log(item)
-          if (key > 6 && key < 12) return true;
-        });
-        dataFiltred = dataFiltred.map((item: any) =>
-          item.completed === false ? "-10" : "10"
-        );
-        // Return values 10 or -10
-        this.lineChartData = [
-          {
-            data: [...dataFiltred],
-            label: "this is an updated label"
+  ngOnInit() {}
+
+  ngAfterViewInit() {
+    this.http.get(this.url).subscribe((todos: any) => {
+      let dataFiltred: any = todos.filter((item, key) => {
+        // console.log(item)
+        if (key > 6 && key < 12) return true;
+      });
+      dataFiltred = dataFiltred.map((item: any) =>
+        item.completed === false ? "-10" : "10"
+      );
+      // Return values 10 or -10
+      //...dataFiltred
+      const ctx = (<HTMLCanvasElement>this.myChart.nativeElement).getContext(
+        "2d"
+      );
+      const gradientStroke = ctx.createLinearGradient(500, 0, 100, 0);
+      const purple_orange_gradient = ctx.createLinearGradient(0, 0, 0, 600);
+      purple_orange_gradient.addColorStop(1, "rgba(199, 150, 239, 0.1)");
+      purple_orange_gradient.addColorStop(0, "rgba(199, 150, 239, 1)");
+  
+      const bar_chart = new Chart(ctx, {
+        type: "line",
+        data: {
+          labels: ["A", "B", "C", "D"],
+          datasets: [
+            {
+              label: "# of Votes",
+              data: [...dataFiltred],
+              backgroundColor: purple_orange_gradient,
+              hoverBackgroundColor: purple_orange_gradient,
+              hoverBorderWidth: 2,
+              hoverBorderColor: "purple",
+              borderWidth: 5,
+              borderColor: "#a77efc"
+            }
+          ]
+        },
+        options: {
+          scales: {
+            yAxes: [
+              {
+                ticks: {
+                  beginAtZero: true
+                }
+              }
+            ]
           }
-        ];
-      })
+        }
+      });
+    });
+
+    // initialise the chart
+
     
   }
 }
